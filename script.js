948adb315a1fe37506ee8e203e5427eef2919ca9
HollowTutoriais <wollowtutoriaisi@gmail.com>
2021-03-30 17:23:16 -0300

    alinhamento
    yey

--- script.js
+++ script.js
@@ -35,18 +35,13 @@ async function checkGameOver() {
 
 	var winner = "";
 
-
-	if((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != ""))
-    winner = a1;
-
-	else if((b2 == b1 && b2 == b3 && b2 !="") || (b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 == c1 && b2 != ""))
-
-	winner = b2;
-	else if((c3 == c2 && c3 == c1 && c3 != "") || (c3 == a3 && c3 == b3 && c3 != "")) 
-
-	winner = c3;
-
-	if (winner != "") {
+	if ((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != "")) {
+	    winner = a1;
+	} else if((b2 == b1 && b2 == b3 && b2 !="") || (b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 == c1 && b2 != "")) {
+		winner = b2;
+	} else if((c3 == c2 && c3 == c1 && c3 != "") || (c3 == a3 && c3 == b3 && c3 != "")) {
+		winner = c3;
+	} if (winner != "") {
 
 		await sleep(50)
 		alert("O gahador foi o:'" + winner + "")
